293b5fc80adf72c788c5feb813da7cf23f0b2905
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-04-27 20:18:31 -0300

    :books: estudando
    :books: estudando

---
+++ java-anatomia-classe/src/edu/andre/tiposevariaveis/TiposVariaveis.java
@@ -0,0 +1,14 @@
+package edu.andre.tiposevariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+
+        // Tipos Primitivos e a classe String
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // assim é uma adaptacao
+        
+    }
+    
+}
